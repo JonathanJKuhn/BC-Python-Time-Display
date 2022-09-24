ced85d8066f7d93436e4be4d0c4d2a2ba1ccae4b
Jonathan Kuhn <jkuhn96@gmail.com>
2022-09-23 22:41:41 -0400

    Changed to localtime()

--- Time_Display_Project/time_display/views.py
+++ Time_Display_Project/time_display/views.py
@@ -1,11 +1,12 @@
+from email.utils import localtime
 from django.shortcuts import render
-from time import gmtime, strftime
+from time import localtime, strftime
 
 # Create your views here.
 def show(request):
     context = {
         'title': 'Date and Time',
-        'date': strftime("%b %d, %Y", gmtime()),
-        'time': strftime("%H:%M %p", gmtime()),
+        'date': strftime("%b %d, %Y", localtime()),
+        'time': strftime("%I:%M %p", localtime()),
     }
     return render(request, 'index.html', context)
